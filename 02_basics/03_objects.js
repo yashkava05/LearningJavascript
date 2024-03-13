669a085964232f6d09d6a2dee273cc9373a66bc6
Yash Kava <110620608+yashkava05@users.noreply.github.com>
2024-03-13 05:10:29 +0000

    object basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,9 @@
+//object literals
+const user1 = {
+    name:"yash", 
+    age:"21",
+    location:"mumbai",
+    email:"yash@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["monday", "tuesday"]
+}
